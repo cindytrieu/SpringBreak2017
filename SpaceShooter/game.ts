2bbbcf902bf23eead368acd3540b96cebdde283a
Mark Gardner <m15399@gmail.com>
2017-03-15 13:38:30 -0700

    Add canvas element

---
+++ SpaceShooter/game.ts
@@ -0,0 +1,46 @@
+
+// Define a shorter name for 'CanvasRenderingContext2d'
+//
+type gContext = CanvasRenderingContext2D;
+
+// Game class
+//
+class Game {
+	
+	static width = 800;
+	static height = 600;
+
+	canvas: HTMLCanvasElement;
+	graphicsContext: gContext;
+
+	testBlock: TestBlock;
+
+	constructor() {
+		
+		// Set up canvas
+		//
+		this.canvas = document.getElementsByTagName('canvas')[0];
+		this.canvas.width = Game.width;
+		this.canvas.height = Game.height;
+
+		this.graphicsContext = this.canvas.getContext('2d');
+
+		// Create a test block
+		//
+		this.testBlock = new TestBlock();
+	}
+
+	update() {
+		this.testBlock.update();
+	}
+
+	draw() {
+		let g = this.graphicsContext;
+
+		// Clear screen
+		g.fillStyle = '#ccc';
+		g.fillRect(0, 0, Game.width, Game.height);
+
+		this.testBlock.draw(g);
+	}
+}
--- SpaceShooter/main.ts
+++ SpaceShooter/main.ts
@@ -4,4 +4,18 @@ AUTHORS: Cindy Trieu, Mark Gardner
 COPYRIGHT 2017
 */
 
-document.write('Hello world!');
+let game: Game;
+
+window.onload = function(){
+
+	// Create the game
+	//
+	game = new Game();
+
+	// Main loop
+	//
+	window.setInterval(function(){
+		game.update();
+		game.draw();
+	}, 1000/30);	
+}
